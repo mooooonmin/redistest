4517650a337df8ecadd300559e98458f478e11e5
Minhee Moon <minhee610@gmail.com>
2023-11-27 14:56:14 +0900

    Remove: All

--- src/main/java/com/example/redistest/entity/LogFacade.java
+++
@@ -1,9 +0,0 @@
-package com.example.redistest.entity;
-
-import com.example.redistest.entity.Log;
-import java.util.List;
-
-public interface LogFacade {
-    List<Log> findAllOrderByDateAtDesc();
-    void removeAll();
-}
